aab4eb72f369ed02ae6706c40c60b724e08bccc6
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-25 21:43:51 -0600

    commit tree 2d353348ae658e93bdbb5396e9b7ddb377a480ee

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(42 - pickup);
+}
+
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(pickup, dropoff) {
+    return Math.abs(pickup - dropoff) * 264;
+}
+
+function calculatesFarePrice(pickup, dropoff) {
+    const dist = distanceTravelledInFeet(pickup, dropoff);
+    if (dist <= 400){
+        return 0;
+    }
+    else if (dist > 400 && dist <= 2000) {
+        return (dist - 400) * 0.02;
+    }
+    else if (dist > 2000 && dist <=2500) {
+        return 25;
+    }
+    else return 'cannot travel that far';
+}
